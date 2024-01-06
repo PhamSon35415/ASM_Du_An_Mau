2589e6e24a869af5d955b17f1ab8c225410dac10
PhamSon35415 <137911617+PhamSon35415@users.noreply.github.com>
2024-01-06 22:30:04 +0700

    Bổ sung giao điện quản lý

---
+++ Du_An_Mau/src/service/chuyendenSV.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+import java.util.*;
+import java.sql.*;
+import model.chuyen_de;
+/**
+ *
+ * @author Admin
+ */
+public class chuyendenSV {
+    List<chuyen_de> list;
+    private Connection conn = null;
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+    private String sql = null;
+    
+}
